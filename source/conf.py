48a575103faf697118fed92ce9943a34433eefac
Brian King <brianjosephking@gmail.com>
2016-01-21 15:01:55 -0600

    adding sphinx-git changelog

--- source/conf.py
+++ source/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
+    'sphinx_git',	
 ]
 
 # Add any paths that contain templates here, relative to this directory.
